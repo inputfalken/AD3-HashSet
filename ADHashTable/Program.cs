3c23d0eaf9bb9bae8094c151e8260b776df39143
Robert Andersson <inputfalken@gmail.com>
2016-10-05 14:08:31 +0200

    removes usage of resource file

--- ADHashTable/Program.cs
+++ ADHashTable/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ADHashTable {
     class Program {
-        static void Main(string[] args) {}
+        static void Main(string[] args) {
+
+        }
+
+        private static string ExtractWords(string word) {
+            return new string(word.Trim('-', ',', '.').ToCharArray().Where(char.IsLetter).ToArray());
+        }
     }
 }
