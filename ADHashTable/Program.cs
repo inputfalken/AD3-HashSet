caca486cd56271876aa2cc474633f80c6709e5d3
Robert Andersson <inputfalken@gmail.com>
2016-10-05 15:59:40 +0200

    WordExtracter now reads the file in a streamreader

--- ADHashTable/Program.cs
+++ ADHashTable/Program.cs
@@ -8,10 +8,40 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ADHashTable {
-    class Program {
-        static void Main(string[] args) {
-
+    internal static class Program {
+        private static void Main(string[] args) {
+            var wordExtracter = new WordExtracter("boye.txt");
+            foreach (var wordExtracterWord in wordExtracter.Words) {
+                Console.WriteLine(wordExtracterWord);
+            }
         }
+    }
+
+    class WordExtracter {
+        private string FilePath { get; }
+
+        public List<string> Words { get; }
+
+        public WordExtracter(string fileName) {
+            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+            if (directoryInfo != null) FilePath = directoryInfo.FullName + $@"\{fileName}";
+
+            Words = new List<string>();
+            using (var reader = new StreamReader(FilePath, Encoding.Default)) {
+                var line = "";
+                while (line != null) {
+                    if (line != "") {
+                        foreach (var word in line.Split(' ')) {
+                            if (word != string.Empty) {
+                                Words.Add(new string(word.ToCharArray().Where(char.IsLetter).ToArray()));
+                            }
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
 
         private static string ExtractWords(string word) {
             return new string(word.Trim('-', ',', '.').ToCharArray().Where(char.IsLetter).ToArray());
