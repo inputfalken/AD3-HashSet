342f2d1590c909aaefce4c89b1f8f369b85753d4
Robert Andersson <inputfalken@gmail.com>
2016-10-06 14:48:52 +0200

    WordExtracter to seperate file

--- ADHashTable/Program.cs
+++ ADHashTable/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.WebSockets;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -14,37 +10,6 @@ namespace ADHashTable {
             foreach (var wordExtracterWord in wordExtracter.Words) {
                 Console.WriteLine(wordExtracterWord);
             }
-        }
-    }
-
-    class WordExtracter {
-        private string FilePath { get; }
-
-        public List<string> Words { get; }
-
-        public WordExtracter(string fileName) {
-            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
-            if (directoryInfo != null) FilePath = directoryInfo.FullName + $@"\{fileName}";
-
-            Words = new List<string>();
-            using (var reader = new StreamReader(FilePath, Encoding.Default)) {
-                var line = "";
-                while (line != null) {
-                    if (line != "") {
-                        foreach (var word in line.Split(' ')) {
-                            if (word != string.Empty) {
-                                Words.Add(new string(word.ToCharArray().Where(char.IsLetter).ToArray()));
-                            }
-                        }
-                    }
-                    line = reader.ReadLine();
-                }
-            }
-        }
-
-
-        private static string ExtractWords(string word) {
-            return new string(word.Trim('-', ',', '.').ToCharArray().Where(char.IsLetter).ToArray());
         }
     }
 }
---
+++ ADHashTable/WordExtracter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ADHashTable {
+    class WordExtracter {
+        private string FilePath { get; }
+
+        public List<string> Words { get; }
+
+        public WordExtracter(string fileName) {
+            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+            if (directoryInfo != null) FilePath = directoryInfo.FullName + $@"\{fileName}";
+
+            Words = new List<string>();
+            using (var reader = new StreamReader(FilePath, Encoding.Default)) {
+                var line = "";
+                while (line != null) {
+                    if (line != "") {
+                        foreach (var word in line.Split(' ')) {
+                            if (word != string.Empty) {
+                                Words.Add(new string(word.ToCharArray().Where(char.IsLetter).ToArray()));
+                            }
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+
+        private static string ExtractWords(string word) {
+            return new string(word.Trim('-', ',', '.').ToCharArray().Where(char.IsLetter).ToArray());
+        }
+    }
+}
