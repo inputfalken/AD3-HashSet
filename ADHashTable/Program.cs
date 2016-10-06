68119b1c3d372348ebc0dfc5f85c941ea05af1ff
Robert Andersson <inputfalken@gmail.com>
2016-10-06 15:26:09 +0200

    updates usage

--- ADHashTable/Program.cs
+++ ADHashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 namespace ADHashTable {
     internal static class Program {
         private static void Main(string[] args) {
-            var wordExtracter = new WordExtracter("boye.txt");
-            foreach (var wordExtracterWord in wordExtracter.Words) {
+            var wordExtracter = new WordExtracter("boye.txt",
+                new HashSet<string>(StringComparer.InvariantCultureIgnoreCase));
+            foreach (var wordExtracterWord in wordExtracter.Collection) {
                 Console.WriteLine(wordExtracterWord);
             }
         }
