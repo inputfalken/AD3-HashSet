10c9b3e0242615e0a604ef34d535cbe7d1055220
Robert Andersson <inputfalken@gmail.com>
2016-10-06 15:25:45 +0200

    Constructor in wordextracter now takes a ICollection

--- ADHashTable/WordExtracter.cs
+++ ADHashTable/WordExtracter.cs
@@ -5,33 +5,34 @@ using System.Text;
 
 namespace ADHashTable {
     internal class WordExtracter {
+        public ICollection<string> Collection { get; }
         private string FilePath { get; }
 
-        public List<string> Words { get; }
 
-        public WordExtracter(string fileName) {
+        public WordExtracter(string fileName, ICollection<string> collection) {
+            Collection = collection;
             var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
             if (directoryInfo != null) FilePath = directoryInfo.FullName + $@"\{fileName}";
+            PopulateCollection();
+        }
 
-            Words = new List<string>();
+        private void PopulateCollection() {
             using (var reader = new StreamReader(FilePath, Encoding.Default)) {
                 var line = "";
                 while (!reader.EndOfStream) {
                     if (line != "") {
-                        foreach (var word in line.Split(' ')) {
-                            if (word != string.Empty) {
-                                Words.Add(new string(word.ToCharArray().Where(char.IsLetter).ToArray()));
+                        if (line != null)
+                            foreach (var word in line.Split(' ')) {
+                                if (word != string.Empty) {
+                                    Collection.Add(ExtractWords(word));
+                                }
                             }
-                        }
                     }
                     line = reader.ReadLine();
                 }
             }
         }
 
-
-        private static string ExtractWords(string word) {
-            return new string(word.Trim('-', ',', '.').ToCharArray().Where(char.IsLetter).ToArray());
-        }
+        private static string ExtractWords(string word) => new string(word.ToCharArray().Where(char.IsLetter).ToArray());
     }
 }
