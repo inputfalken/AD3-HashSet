116b78d14886cac910d97c12316b1d91561fc0c0
Robert Andersson <inputfalken@gmail.com>
2016-10-06 15:32:20 +0200

    Program now makes a text analysis on words of two textfiles.

--- ADHashTable/Program.cs
+++ ADHashTable/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -7,10 +8,15 @@ using System.Threading.Tasks;
 namespace ADHashTable {
     internal static class Program {
         private static void Main(string[] args) {
-            var wordExtracter = new WordExtracter("boye.txt",
+            var boyeSet = new WordExtracter("boye.txt",
                 new HashSet<string>(StringComparer.InvariantCultureIgnoreCase));
-            foreach (var wordExtracterWord in wordExtracter.Collection) {
-                Console.WriteLine(wordExtracterWord);
+            var commonWordSet = new WordExtracter("ordlista.txt",
+                new HashSet<string>(StringComparer.InvariantCultureIgnoreCase));
+
+            foreach (var word in boyeSet.Collection) {
+                if (!commonWordSet.Collection.Contains(word)) {
+                    Console.WriteLine(word);
+                }
             }
         }
     }
